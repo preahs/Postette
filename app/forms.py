5485e0a84dc3165d0477e4eed9d5317a741cee24
preahs <93487975+preahs@users.noreply.github.com>
2025-06-09 10:45:04 -0500

    Fixed /create route to properly import PostForm

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, MultipleFileField, SubmitField, SelectMultipleField
+from wtforms import StringField, TextAreaField, SubmitField, MultipleFileField
 from wtforms.validators import DataRequired
-from flask_wtf.file import FileAllowed
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
-    groups = SelectMultipleField('Groups', coerce=int)
-    images = MultipleFileField('Images', validators=[FileAllowed(['jpg', 'jpeg', 'png', 'gif'], 'Images only!')])
-    submit = SubmitField('Create Post')
+    images = MultipleFileField('Images')
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,7 @@ from . import db
 import os
 from datetime import datetime
 from flask import current_app
+from .forms import PostForm
 
 main = Blueprint('main', __name__)
 
@@ -17,36 +18,24 @@ def index():
 # Create a new post
 @main.route('/create', methods=['GET', 'POST'])
 def create_post():
-    groups = Group.query.all()
+    form = PostForm()
+    if form.validate_on_submit():
+        image_filenames = []
+        for image in form.images.data:
+            if image:
+                filename = secure_filename(image.filename)
+                image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+                image_filenames.append(filename)
 
-    if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
-        group_ids = request.form.getlist('groups')
-
-        post = Post(title=title, content=content, timestamp=datetime.utcnow())
-        
-        for group_id in group_ids:
-            group = Group.query.get(group_id)
-            if group:
-                post.groups.append(group)
-
-        # Handle file uploads
-        if 'images' in request.files:
-            files = request.files.getlist('images')
-            upload_dir = os.path.join(current_app.instance_path, 'uploads')
-            os.makedirs(upload_dir, exist_ok=True)
-
-            for file in files:
-                if file.filename:
-                    filename = secure_filename(file.filename)
-                    filepath = os.path.join(upload_dir, filename)
-                    file.save(filepath)
-
+        post = Post(
+            title=form.title.data,
+            content=form.content.data,
+            image_filenames=",".join(image_filenames)
+        )
         db.session.add(post)
         db.session.commit()
 
-        flash('Post created successfully!', 'success')
+        flash("Post created successfully!", "success")
         return redirect(url_for('main.index'))
 
-    return render_template('create_post.html', groups=groups)
+    return render_template('create_post.html', form=form)
